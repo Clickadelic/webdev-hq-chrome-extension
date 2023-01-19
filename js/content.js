e11284afb1b32375ca9d8d51836e4f6e929fd8e9
Clickadelic <toby.hopp@gmail.com>
2023-01-19 05:10:21 +0100

    TailWind rebuild

--- js/content.js
+++ js/content.js
@@ -1,90 +1,87 @@
-// class Actionpanel {
+class WebDevBox {
 
-// 	constructor(){
-// 		this.addInlineStyle()
-// 		this.addHtml()
-// 	}
+	constructor(){
+		this.addInlineStyle()
+		this.addHtml()
+		this.addActions()
+	}
 
-// 	addInlineStyle(){
-// 		const style = document.createElement('style')
-// 		const head = document.querySelector('head')
-// 		style.setAttribute('id', 'webdev-hq-actionpanel-css')
-// 		// CSS
-// 		style.textContent = `
-// 			:root {
-// 				--wdhq-white: #ffffff;
-// 				--wdhq-dashboard-bg: #f5f5f5;
-// 				--wdhq-primary: #1890ff;
-// 				--wdhq-box-shadow: rgb(0 0 0 / 8%) 0px 1px 4px;
-// 				--wdhq-box-border: 1px solid rgb(230, 235, 241);
-// 			}
-// 			#webdev-hq-actionpanel {
-// 				position: fixed;
-// 				left: 50%;
-//     			transform: translateX(-50%);
-// 				width: 1320px;
-// 				height: 300px;
-// 				margin: 0 auto;
-// 				background: #f5f5f5;
-// 				border-top-left-radius: 4px;
-// 				border-top-right-radius: 4px;
-// 				border: var(--wdhq-box-border);
-// 				z-index: 1100;
-// 			}
-// 			.wdhq-panel-closed {
-// 				bottom: -300px;
-// 			}
-// 			.wdhq-panel-open {
-// 				bottom: 0;
-// 			}
-// 			#btn-panel-toggle {
-// 				position: fixed;
-// 				right: 2rem;
-// 				bottom: 2rem;
-// 				color: var(--wdhq-white);
-// 				background-color:var(--wdhq-primary);
-// 				padding: 1rem;
-// 				border: 0;
-// 				border-radius: 50%;
-// 				box-shadow: var(--wdhq-box-shadow);
-// 			}
-// 			#btn-panel-toggle svg {
-// 				position: relative;
-// 				top: 3px;
-// 			}
-// 		`;
-// 		head.appendChild(style)
-// 	}
+	addInlineStyle(){
+		const style = document.createElement('style')
+		const head = document.querySelector('head')
+		style.setAttribute('id', 'webdev-hq-inline-style')
+		// CSS
+		style.textContent = `
+			:root {
+				--wdhq-white: #ffffff;
+				--wdhq-dashboard-bg: #f5f5f5;
+				--wdhq-primary: #3b82f6;
+				--wdhq-accent: #f63b4e;
+				--wdhq-box-shadow: rgb(0 0 0 / 8%) 0px 1px 4px;
+				--wdhq-box-border: 1px solid rgb(230, 235, 241);
+			}
+			#webdev-hq-box {
+				position: fixed;
+				top: 50%;
+				right: 0;
+				background: transparent;
+				transform: translatey(-50%);
+				border-top-left-radius: 4px;
+				border-bottom-left-radius: 4px;
+				padding: 0;
+				margin: 0;
+			}
+			#collect-btn {
+				background: var(--wdhq-primary);
+				border: none;
+				color: var(--wdhq-white);
+				padding: .5rem;
+				border-top-left-radius: 4px;
+				border-bottom-left-radius: 4px;
+			}
+			#collect-btn svg {
+				position: relative;
+				top: 4px;
+				margin: .5rem;
+			}
+			#collect-btn:hover {
+				background: red;
+				cursor: pointer;
+			}
 
-// 	addHtml(){
+		`;
+		head.appendChild(style)
+	}
 
-// 		const body = document.querySelector('body')
-// 		const panel = document.createElement('div')
-// 		const contentBox = document.createElement('div')
-// 		const toggleBtn = document.createElement('button')
-// 		const btnIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-gear" viewBox="0 0 16 16"><path d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"/><path d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"/></svg>`
-// 		toggleBtn.innerHTML = btnIcon
+	addHtml(){
+		// Setup
+		const body = document.querySelector('body')
+		const devbox = document.createElement('div')
+		const collectBtn = document.createElement('button')
 
-// 		// The Panel
-// 		toggleBtn.setAttribute('id', 'btn-panel-toggle')
-// 		toggleBtn.addEventListener('click', () => {
-// 			if(panel.classList.contains('wdhq-panel-closed')){
-// 				panel.classList.remove('wdhq-panel-closed')
-// 				panel.classList.add('wdhq-panel-open')
-// 			} else {
-// 				panel.classList.remove('wdhq-panel-open')
-// 				panel.classList.add('wdhq-panel-closed')
-// 			}
-// 		})
+		collectBtn.setAttribute('id', 'collect-btn')
+		const saveIcon = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-bookmark-plus" viewBox="0 0 16 16"><path d="M2 2a2 2 0 0 1 2-2h8a2 2 0 0 1 2 2v13.5a.5.5 0 0 1-.777.416L8 13.101l-5.223 2.815A.5.5 0 0 1 2 15.5V2zm2-1a1 1 0 0 0-1 1v12.566l4.723-2.482a.5.5 0 0 1 .554 0L13 14.566V2a1 1 0 0 0-1-1H4z"/><path d="M8 4a.5.5 0 0 1 .5.5V6H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V7H6a.5.5 0 0 1 0-1h1.5V4.5A.5.5 0 0 1 8 4z"/></svg>`
+		collectBtn.innerHTML = saveIcon
+		devbox.setAttribute('id', 'webdev-hq-box')
 
-// 		panel.setAttribute('id', 'webdev-hq-actionpanel')
-// 		panel.classList.add('wdhq-content-box', 'wdhq-panel-closed')
-// 		body.appendChild(panel)
-// 		body.appendChild(toggleBtn)
-// 	}
 
-// }
 
-// const myBox = new Actionpanel()
+		// Add to divbox
+		devbox.appendChild(collectBtn)
+		
+		// add everything
+		body.appendChild(devbox)
+	}
 
-alert('Hello, world!');
+	addActions(){
+		const collectBtn = document.getElementById('collect-btn')
+		collectBtn.addEventListener('click', () => {
+			alert("ConJulio")
+		})
+	}
+
+}
+
+const myBox = new WebDevBox()
+
+// alert('Hello, world!');
