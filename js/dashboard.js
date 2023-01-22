1af7170ba8af0a411c8547c0bba5ebaadcc78397
clickadelic <toby.hopp@gmail.com>
2023-01-22 01:34:12 +0100

    red, green toggle

--- js/dashboard.js
+++ js/dashboard.js
@@ -2,10 +2,19 @@ function dashboardSetup(){
 	const toggleBtn = document.getElementById("btn-sidebar-toggle")
 	toggleBtn.addEventListener("click", toggleSidebar)
 }
-
 dashboardSetup()
 
 function toggleSidebar(){
+
+	const sidebarText = document.getElementsByClassName("sidebar-text")
+	const navbarLogo = document.getElementById("App-logo")
 	const sidebar = document.getElementById("App-sidebar")
-	sidebar.classList.toggle("md:block")
+	const navbar = document.getElementById("App-navbar")
+
+	if(sidebar.style.backgroundColor == "green"){
+		sidebar.style.backgroundColor = "red"
+	} else {
+		sidebar.style.backgroundColor = "green"
+	}
+
 }
