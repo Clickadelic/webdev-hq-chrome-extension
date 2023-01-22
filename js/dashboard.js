7e41929b3971f27a10e4e9f3b685f2b701723881
clickadelic <toby.hopp@gmail.com>
2023-01-22 01:34:54 +0100

    Minimum color toggle

--- js/dashboard.js
+++ js/dashboard.js
@@ -5,16 +5,11 @@ function dashboardSetup(){
 dashboardSetup()
 
 function toggleSidebar(){
-
-	const sidebarText = document.getElementsByClassName("sidebar-text")
-	const navbarLogo = document.getElementById("App-logo")
+	
 	const sidebar = document.getElementById("App-sidebar")
-	const navbar = document.getElementById("App-navbar")
-
 	if(sidebar.style.backgroundColor == "green"){
 		sidebar.style.backgroundColor = "red"
 	} else {
 		sidebar.style.backgroundColor = "green"
 	}
-
 }
