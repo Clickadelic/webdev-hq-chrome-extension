4caa307aded14822c3b08e36b17200c0ebe30992
Clickadelic <toby.hopp@gmail.com>
2023-01-28 20:29:40 +0100

    CleanUp

--- js/background.js
+++ js/background.js
@@ -8,3 +8,13 @@ chrome.runtime.onMessage.addListener(
 	}
 );
 
+// Log current tabs
+async function getCurrentTab() {
+	let queryOptions = { active: true, lastFocusedWindow: true }
+	// `tab` will either be a `tabs.Tab` instance or `undefined`.
+	let [tab] = await chrome.tabs.query(queryOptions)
+	console.log("GetCurrentTab", tab)
+	return tab
+}
+
+getCurrentTab()
