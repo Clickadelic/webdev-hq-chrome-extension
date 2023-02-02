b66ef4bfe956574fe37fe6dacbaa83bb48724a24
clickadelic <toby.hopp@gmail.com>
2023-02-03 00:47:28 +0100

    Digital Clock added

--- js/home.js
+++ js/home.js
@@ -3,6 +3,8 @@ function homescreenSetup(){
 	toggleBtn.addEventListener("click", toggleSidebar)
 	const megaMenuBtn = document.getElementById("toggle-mega-menu")
 	megaMenuBtn.addEventListener("click", toggleMegaMenu)
+	const langBtn = document.getElementById("language-menu")
+	langBtn.addEventListener("click", togglelanguageMenu)
 }
 homescreenSetup()
 
@@ -10,13 +12,16 @@ function toggleSidebar(){
 	const sidebar = document.getElementById("App-sidebar")
 	const header = document.getElementById("App-header")
 	const sidebarTexts = document.getElementsByClassName("sidebar-text")
-
+	const main = document.getElementById("main-content")
 	if((sidebar.classList.contains("w-64")) && (header.classList.contains("ml-64"))){
 		sidebar.classList.remove("w-64")
 		header.classList.remove("ml-64")
 
 		sidebar.classList.add("w-16")
 		header.classList.add("ml-16")
+
+		main.classList.remove("ml-48")
+		main.classList.add("ml-24")
 		for (let item of sidebarTexts) {
 			item.classList.add("hidden")
 		}
@@ -26,6 +31,9 @@ function toggleSidebar(){
 
 		sidebar.classList.add("w-64")
 		header.classList.add("ml-64")
+
+		main.classList.add("ml-48")
+		main.classList.remove("ml-24")
 		for (let item of sidebarTexts) {
 			item.classList.remove("hidden")
 		}
@@ -42,3 +50,50 @@ function toggleMegaMenu(){
 		megaMenu.classList.remove("grid")
 	}
 }
+
+function togglelanguageMenu() {
+	const languageMenu = document.getElementById("language-dropdown")
+	if(languageMenu.classList.contains("hidden")){
+		languageMenu.classList.remove("hidden")
+	} else {
+		languageMenu.classList.add("hidden")
+	}
+}
+
+function toggleaccountMenu() {
+	const accountMenu = document.getElementById("account-dropdown")
+	if(accountMenu.classList.contains("hidden")){
+		accountMenu.classList.remove("hidden")
+	} else {
+		accountMenu.classList.add("hidden")
+	}
+}
+
+function actionSearchbla(){
+	let term = '';
+
+	alert(`https://www.google.com/search?q=${term}`)
+}
+
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+
+    var session = "AM";
+    
+    
+    
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+
+    
+    var time = h + ":" + m;
+    document.getElementById("digital-clock").innerText = time;
+    document.getElementById("digital-clock").textContent = time;
+    
+    setTimeout(showTime, 1000);
+    
+}
+
+showTime();
