54a1d71f801797e7d5e1c8833cfbf47105de7cb5
Clickadelic <toby.hopp@gmail.com>
2023-04-09 03:08:33 +0200

    History list is working

--- src/newTab/NewTab.js
+++ src/newTab/NewTab.js
@@ -2,21 +2,19 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import '../assets/css/tailwind.css';
 import History from './components/History';
-import DigitalClock from './components/DigitalClock';
 import About from './components/About';
 import SearchBox from './components/SearchBox';
 function NewTab() {
     return (React.createElement("div", { className: "App flex h-screen bg-slate-100 bg-window-wall" },
         React.createElement("div", { className: "m-auto px-4 md:w-full" },
             React.createElement(SearchBox, null),
-            React.createElement(DigitalClock, null),
             React.createElement("div", { className: "tab-box md:w-[800px] m-auto p-4 rounded bg-white/30 backdrop-blur-md" },
                 React.createElement("nav", { className: "tab-nav" },
-                    React.createElement("ul", { className: "flex bg-white" },
-                        React.createElement("li", { className: "list-none" },
-                            React.createElement("a", { href: "#/", className: "p-2 text-medium" }, "History")),
-                        React.createElement("li", { className: "list-none" },
-                            React.createElement("a", { href: "#/about", className: "p-2 text-medium" }, "About")))),
+                    React.createElement("ul", { className: "flex bg-white list-none" },
+                        React.createElement("li", null,
+                            React.createElement("a", { href: "#/", className: "inline-block m-2 p-2 text-2xl text-slate-500" }, "History")),
+                        React.createElement("li", null,
+                            React.createElement("a", { href: "#/about", className: "inline-block m-2 p-2 text-2xl text-slate-500" }, "About")))),
                 React.createElement("main", { className: "md:w-full bg-white" },
                     React.createElement(Routes, null,
                         React.createElement(Route, { path: "/", element: React.createElement(History, null) }),
--- src/newTab/NewTab.tsx
+++ src/newTab/NewTab.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import '../assets/css/tailwind.css'
 import History from './components/History'
-import DigitalClock from './components/DigitalClock'
 import About from './components/About'
 import SearchBox from './components/SearchBox'
 
@@ -11,12 +10,11 @@ function NewTab() {
 		<div className="App flex h-screen bg-slate-100 bg-window-wall">
 			<div className="m-auto px-4 md:w-full">
 				<SearchBox />
-				<DigitalClock />
 				<div className="tab-box md:w-[800px] m-auto p-4 rounded bg-white/30 backdrop-blur-md">
 					<nav className="tab-nav">
-						<ul className="flex bg-white">
-							<li className="list-none"><a href="#/" className="p-2 text-medium">History</a></li>
-							<li className="list-none"><a href="#/about" className="p-2 text-medium">About</a></li>
+						<ul className="flex bg-white list-none">
+							<li><a href="#/" className="inline-block m-2 p-2 text-2xl text-slate-500">History</a></li>
+							<li><a href="#/about" className="inline-block m-2 p-2 text-2xl text-slate-500">About</a></li>
 						</ul>
 					</nav>
 					<main className="md:w-full bg-white">
