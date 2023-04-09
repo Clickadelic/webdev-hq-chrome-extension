c690214538285b676345886ec8cb0e915c09a9ef
Clickadelic <toby.hopp@gmail.com>
2023-04-09 04:13:41 +0200

    History buttons..

--- src/newTab/NewTab.js
+++ src/newTab/NewTab.js
@@ -5,17 +5,17 @@ import History from './components/History';
 import About from './components/About';
 import SearchBox from './components/SearchBox';
 function NewTab() {
-    return (React.createElement("div", { className: "App flex h-screen bg-slate-100 bg-window-wall" },
-        React.createElement("div", { className: "m-auto px-4 md:w-full" },
+    return (React.createElement("div", { className: "App h-screen bg-slate-100 bg-color-fog" },
+        React.createElement("div", { className: "m-auto px-4 pt-24 md:w-full" },
             React.createElement(SearchBox, null),
-            React.createElement("div", { className: "tab-box md:w-[800px] m-auto p-4 rounded bg-white/30 backdrop-blur-md" },
-                React.createElement("nav", { className: "tab-nav" },
-                    React.createElement("ul", { className: "flex bg-white list-none" },
+            React.createElement("div", { className: "tab-box md:w-[800px] m-auto" },
+                React.createElement("nav", { className: "tab-nav p-4 mb-4 rounded-full bg-white/30 backdrop-blur-md" },
+                    React.createElement("ul", { className: "flex rounded-full bg-white list-none" },
                         React.createElement("li", null,
-                            React.createElement("a", { href: "#/", className: "inline-block m-2 p-2 text-2xl text-slate-500" }, "History")),
+                            React.createElement("a", { href: "#/", className: "inline-block m-2 ml-6 p-2 text-base text-slate-500" }, "History")),
                         React.createElement("li", null,
-                            React.createElement("a", { href: "#/about", className: "inline-block m-2 p-2 text-2xl text-slate-500" }, "About")))),
-                React.createElement("main", { className: "md:w-full bg-white" },
+                            React.createElement("a", { href: "#/about", className: "inline-block m-2 p-2 text-base text-slate-500" }, "About")))),
+                React.createElement("main", { className: "md:w-full p-4 mb-4" },
                     React.createElement(Routes, null,
                         React.createElement(Route, { path: "/", element: React.createElement(History, null) }),
                         React.createElement(Route, { path: "/about", element: React.createElement(About, null) })))))));
--- src/newTab/NewTab.tsx
+++ src/newTab/NewTab.tsx
@@ -7,17 +7,17 @@ import SearchBox from './components/SearchBox'
 
 function NewTab() {
 	return (
-		<div className="App flex h-screen bg-slate-100 bg-window-wall">
-			<div className="m-auto px-4 md:w-full">
+		<div className="App h-screen bg-slate-100 bg-color-fog">
+			<div className="m-auto px-4 pt-24 md:w-full">
 				<SearchBox />
-				<div className="tab-box md:w-[800px] m-auto p-4 rounded bg-white/30 backdrop-blur-md">
-					<nav className="tab-nav">
-						<ul className="flex bg-white list-none">
-							<li><a href="#/" className="inline-block m-2 p-2 text-2xl text-slate-500">History</a></li>
-							<li><a href="#/about" className="inline-block m-2 p-2 text-2xl text-slate-500">About</a></li>
+				<div className="tab-box md:w-[800px] m-auto">
+					<nav className="tab-nav p-4 mb-4 rounded-full bg-white/30 backdrop-blur-md">
+						<ul className="flex rounded-full bg-white list-none">
+							<li><a href="#/" className="inline-block m-2 ml-6 p-2 text-base text-slate-500">History</a></li>
+							<li><a href="#/about" className="inline-block m-2 p-2 text-base text-slate-500">About</a></li>
 						</ul>
 					</nav>
-					<main className="md:w-full bg-white">
+					<main className="md:w-full p-4 mb-4">
 						<Routes>
 							<Route path="/" element={<History />} />
 							<Route path="/about" element={<About />} />
