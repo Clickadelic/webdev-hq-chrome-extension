f37a37cc1a670dd6a31692676e25e74130cbf147
Clickadelic <toby.hopp@gmail.com>
2023-04-29 22:04:26 +0200

    Strukturelle Änderungen

--- extension/src/newTab/NewTab.js
+++ extension/src/newTab/NewTab.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../assets/css/tailwind.css';
-import MultiBox from './components/MultiBox';
 import NavBar from './components/NavBar';
+import Logo from '../Logo';
+import FormSearchBar from './components/FormSearchBar';
 function NewTab() {
     return (React.createElement("div", { className: "App h-screen bg-slate-900 bg-color-stripes" },
         React.createElement(NavBar, null),
-        React.createElement(MultiBox, null)));
+        React.createElement("div", { className: "flex m-auto md:w-[600px] justify-center mb-12" },
+            React.createElement(Logo, null)),
+        React.createElement("div", { className: "flex m-auto md:w-[600px] justify-center mb-12 bg-white/10 backdrop backdrop-blur p-2" },
+            React.createElement("div", { className: "bg-white p-3 w-full rounded" },
+                React.createElement(FormSearchBar, null)))));
 }
 export default NewTab;
--- extension/src/newTab/NewTab.tsx
+++ extension/src/newTab/NewTab.tsx
@@ -1,16 +1,24 @@
 import React from 'react'
 import {Routes, Route} from 'react-router-dom'
 import '../assets/css/tailwind.css'
+import NavBar from './components/NavBar'
+import Logo from '../Logo'
+import FormSearchBar from './components/FormSearchBar'
 import History from './components/History'
 import About from './components/About'
-import MultiBox from './components/MultiBox'
-import NavBar from './components/NavBar'
-
 function NewTab() {
 	return (
 		<div className="App h-screen bg-slate-900 bg-color-stripes">
 			<NavBar />
-			<MultiBox />
+			<div className="flex m-auto md:w-[600px] justify-center mb-12">
+				<Logo />
+			</div>
+			<div className="flex m-auto md:w-[600px] justify-center mb-12 bg-white/10 backdrop backdrop-blur p-2">
+				<div className="bg-white p-3 w-full rounded">
+					<FormSearchBar />
+				</div>
+			</div>
+			
 			{/* <main className="md:w-full p-4 mb-4">
 				<Routes>
 					<Route path="/" element={<History />} />
