e92282bc0da10e9f8511e6e7684e1dacffab515b
Clickadelic <toby.hopp@gmail.com>
2023-05-20 23:21:49 +0200

    px links

--- extension/src/components/GoogleApps/index.tsx
+++ extension/src/components/GoogleApps/index.tsx
@@ -20,59 +20,59 @@ function GoogleApps({classes}) {
 
 	return (
 		<div className={classes}>
-			<ul className="grid grid-cols-12 gap-4 align-center p-2 rounded-b">
+			<ul className="grid grid-cols-12 gap-4 content-center items-center p-2 rounded-b">
 				<li>
-					<a href="https://docs.google.com/" className="bg-white rounded-full p-1 block" target="_self">
+					<a href="https://docs.google.com/" className="block p-2 w-[32px] h-[32px] bg-white rounded-full" target="_self">
 						<img src="../../static/icons/google-apps/google-docs.svg" width="16" alt="Google Docs" />
 					</a>
 				</li>
 				<li>
-					<a href="https://docs.google.com/spreadsheets/" className="bg-white rounded-full p-1 block" target="_self">
+					<a href="https://docs.google.com/spreadsheets/" className="block p-2 w-[32px] h-[32px] bg-white rounded-full" target="_self">
 						<img src="../../static/icons/google-apps/google-sheets.svg" width="16" alt="Google Sheets" />
 					</a>
 				</li>
 				<li>
-					<a href="https://docs.google.com/slides/" className="bg-white rounded-full p-1 block" target="_self">
+					<a href="https://docs.google.com/slides/" className="block p-2 w-[32px] h-[32px] bg-white rounded-full" target="_self">
 						<img src="../../static/icons/google-apps/google-slides.svg" width="16" alt="Google Slides" />
 					</a>
 				</li>
 				<li>
-					<a href="https://docs.google.com/forms/u/0/?tgif=d" className="bg-white rounded-full p-1 block" target="_self">
+					<a href="https://docs.google.com/forms/u/0/?tgif=d" className="block p-2 w-[32px] h-[32px] bg-white rounded-full" target="_self">
 						<img src="../../static/icons/google-apps/google-forms.svg" width="16" alt="Google Slides" />
 					</a>
 				</li>
 				<li>
-					<a href="https://mail.google.com/" className="bg-white rounded-full p-1 block" target="_self">
+					<a href="https://mail.google.com/" className="block p-2 w-[32px] h-[32px] bg-white rounded-full" target="_self">
 						<img src="../../static/icons/google-apps/google-gmail.svg" width="22" alt="G-Mail" />
 					</a>
 				</li>
 				<li>
-					<a href="https://drive.google.com/drive/my-drive" className="bg-white rounded-full p-1 block" target="_self">
+					<a href="https://drive.google.com/drive/my-drive" className="block p-2 w-[32px] h-[32px] bg-white rounded-full" target="_self">
 						<img src="../../static/icons/google-apps/google-drive.svg" width="22" alt="Google Drive" />
 					</a>
 				</li>
 				<li>
-					<a href="https://calendar.google.com/calendar/u/0/r" className="bg-white rounded-full p-1 block" target="_self">
+					<a href="https://calendar.google.com/calendar/u/0/r" className="block p-2 w-[32px] h-[32px] bg-white rounded-full" target="_self">
 						<img src="../../static/icons/google-apps/google-calendar.svg" width="22" alt="Google Calendar" />
 					</a>
 				</li>
 				<li>
-					<a href="https://www.youtube.com/" className="bg-white rounded-full p-1 block" target="_self">
+					<a href="https://www.youtube.com/" className="block p-2 w-[32px] h-[32px] bg-white rounded-full" target="_self">
 						<img src="../../static/icons/google-apps/youtube-color-icon.svg" width="22" alt="YouTube" />
 					</a>
 				</li>
 				<li>
-					<a href="https://search.google.com/search-console/about" className="bg-white rounded-full p-1 block" target="_self">
+					<a href="https://search.google.com/search-console/about" className="block p-2 w-[32px] h-[32px] bg-white rounded-full" target="_self">
 						<img src="../../static/icons/google-apps/google-search-console.svg" width="22" alt="Google Search Console" />
 					</a>
 				</li>
 				<li>
-					<a href="https://domains.google/" className="bg-white rounded-full p-1 block" target="_self">
+					<a href="https://domains.google/" className="block p-2 w-[32px] h-[32px] bg-white rounded-full" target="_self">
 						<img src="../../static/icons/google-apps/google-domains.svg" width="22" alt="Google Domains" />
 					</a>
 				</li>
 				<li>
-					<a href="https://toolbox.googleapps.com/apps/checkmx/" className="bg-white rounded-full p-1 block" target="_self">
+					<a href="https://toolbox.googleapps.com/apps/checkmx/" className="block p-2 w-[32px] h-[32px] bg-white rounded-full" target="_self">
 						<img src="../../static/icons/google-apps/g-logo.svg" width="22" alt="Google MX Check" />
 					</a>
 				</li>
