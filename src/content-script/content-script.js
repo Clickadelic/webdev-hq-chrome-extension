10a20f142b15f6f9a90db07ca338698f517defff
Clickadelic <toby.hopp@gmail.com>
2023-07-30 04:36:32 +0200

    Button

--- src/content-script/content-script.js
+++ src/content-script/content-script.js
@@ -1,19 +1,32 @@
-class ActionButton {
+class WebDevHQButton {
     constructor() {
         this.createStyleTag();
     }
     createStyleTag() {
+        const css = `
+			.webdev-hq__button {
+				position: fixed;
+				right: 30px;
+				bottom: 30px;
+				border-radius: 4px;
+				background-color: blue;
+				color: white;
+				padding: 2rem;
+			}
+		`;
         const style = document.createElement('style');
         style.setAttribute('type', 'text/css');
         style.setAttribute('id', 'webdev-hq-btn-css');
+        style.innerHTML = css;
         document.head.appendChild(style);
     }
     createElements() {
         const button = document.createElement("button");
         button.innerText = "Save";
+        button.classList.add("webdev-hq__button");
         document.body.appendChild(button);
     }
 }
 window.onload = () => {
-    const webdevhqbtn = new ActionButton();
+    const webdevhqbtn = new WebDevHQButton();
 };
