32bbab8800dfd80d0febea0c644827b7459a371e
Clickadelic <toby.hopp@gmail.com>
2023-12-18 23:04:45 +0100

    Google Apps

--- src/components/Todos/index.tsx
+++ src/components/Todos/index.tsx
@@ -1,32 +1,40 @@
-import React from "react"
-import { useState } from "react"
+import React from "react";
+import { useState } from "react";
 
 function TodoList() {
-	const [todos, setTodos] = useState([])
-	const [inputValue, setInputValue] = useState("")
-	const [isLoading, setIsLoading] = useState(false)
+	const [todos, setTodos] = useState([]);
+	const [inputValue, setInputValue] = useState("");
+	const [isLoading, setIsLoading] = useState(false);
 
 	const handleDelete = index => {
-		const newTodos = [...todos]
-		newTodos.splice(index, 1)
-		setTodos(newTodos)
-	}
+		const newTodos = [...todos];
+		newTodos.splice(index, 1);
+		setTodos(newTodos);
+	};
 
 	function handleChange(e) {
-		setInputValue(e.target.value)
+		setInputValue(e.target.value);
 	}
 
 	function handleSubmit(e) {
-		e.preventDefault()
-		setIsLoading(true)
-		setTodos([...todos, inputValue])
-		setInputValue("")
-		setIsLoading(false)
+		e.preventDefault();
+		if (inputValue == "" || inputValue == undefined || inputValue.length <= 2) {
+			return;
+		}
+		setIsLoading(true);
+		setTodos([...todos, inputValue]);
+		setInputValue("");
+		setIsLoading(false);
+	}
+
+	function toggleDone(e) {
+		e.preventDefault();
+		console.log(inputValue);
 	}
 
 	return (
 		<div className="todo-app m-auto md:w-[760px] justify-between rounded bg-white/10 backdrop backdrop-blur p-2">
-			<form className="bg-white-10 rounded flex justify-between mb-2">
+			<form className="bg-white-10 rounded flex justify-between">
 				<input type="text" value={inputValue} onChange={handleChange} className="p-2 pl-3 text-base rounded w-full mr-2" placeholder={chrome.i18n.getMessage("newTodo")} />
 				<span className="inline-flex justify-between">
 					<button onClick={handleSubmit} className="bg-blue-600 text-white rounded p-3 hover:bg-blue-500">
@@ -39,9 +47,12 @@ function TodoList() {
 			<ul className="todo-list">
 				{isLoading && <span>Loading...</span>}
 				{todos.map((todo, index) => (
-					<li key={index} className="flex justify-between  mb-2">
-						<span className="block w-100 bg-white p-2 rounded-full mr-2">{todo}</span>
-						<button className="backdrop-blur-md bg-red-500 text-white rounded p-3 hover:bg-red-600" onClick={() => handleDelete(index)}>
+					<li key={index} className="flex justify-between mt-2">
+						<div className="flex w-full text-base text-slate-800 bg-white rounded-md p-2 mr-2">
+							<input type="checkbox" onClick={toggleDone} id="done" name="done" className="m-2" />
+							<p className="ml-2">{todo}</p>
+						</div>
+						<button className="bg-red-500 text-white rounded p-3 hover:bg-red-600" onClick={() => handleDelete(index)}>
 							<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash3" viewBox="0 0 16 16">
 								<path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5" />
 							</svg>
@@ -50,7 +61,7 @@ function TodoList() {
 				))}
 			</ul>
 		</div>
-	)
+	);
 }
 
-export default TodoList
+export default TodoList;
--- src/constants/index.js
+++ src/constants/index.js
@@ -4,55 +4,69 @@ export const apps = [
         title: "YouTube",
         icon: "../static/icons/google-apps/youtube-color-icon.svg",
         href: "https://youtube.com",
-        target: "_blank"
+        target: "_self"
     },
     {
         id: "drive",
         title: "Drive",
         icon: "../static/icons/google-apps/google-drive.svg",
         href: "https://www.google.com/drive/",
-        target: "_blank"
+        target: "_self"
     },
     {
         id: "gmail",
         title: "G-Mail",
         icon: "../static/icons/google-apps/google-gmail.svg",
         href: "https://mail.google.com/",
-        target: "_blank"
+        target: "_self"
     },
     {
         id: "docs",
         title: "Docs",
         icon: "../static/icons/google-apps/google-docs.svg",
         href: "https://docs.google.com/document/u/0/",
-        target: "_blank"
+        target: "_self"
     },
     {
         id: "sheets",
         title: "Sheets",
         icon: "../static/icons/google-apps/google-sheets.svg",
         href: "https://docs.google.com/spreadsheets/u/0/",
-        target: "_blank"
+        target: "_self"
     },
     {
         id: "presentations",
         title: "Presentations",
         icon: "../static/icons/google-apps/google-slides.svg",
         href: "https://docs.google.com/presentation/u/0/",
-        target: "_blank"
+        target: "_self"
+    },
+    {
+        id: "forms",
+        title: "Forms",
+        icon: "../static/icons/google-apps/google-forms.svg",
+        href: "https://docs.google.com/forms/u/0/",
+        target: "_self"
     },
     {
         id: "calendar",
         title: "Calendar",
         icon: "../static/icons/google-apps/google-calendar.svg",
         href: "https://calendar.google.com/calendar/u/0/r",
-        target: "_blank"
+        target: "_self"
+    },
+    {
+        id: "contacts",
+        title: "Contacts",
+        icon: "../static/icons/google-apps/google-contacts.svg",
+        href: "https://passwords.google.com",
+        target: "_self"
     },
     {
         id: "passwords",
         title: "Passwords",
         icon: "../static/icons/google-apps/google-passwords.svg",
         href: "https://passwords.google.com",
-        target: "_blank"
+        target: "_self"
     }
 ];
