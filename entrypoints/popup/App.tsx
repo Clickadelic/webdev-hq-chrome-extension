95d4bcb41ceed8392d024824e7f670f9be330116
Tobias Hopp <tobias.hopp@alzura.com>
2025-03-14 23:03:56 +0100

    popup bg

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -2,7 +2,7 @@ import Logo from "@/components/custom/Logo";
 
 const App = () => {
   return (
-    <div className="size-[600px] bg-mantis p-3 pt-8 text-center">
+    <div className="size-[600px] bg-slate-100 p-3 pt-8 text-center">
       <Logo classes="text-3xl" />
     </div>
   );
