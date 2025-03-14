3fd3cc24f4764bc79260b8183a84c6cebfae385c
Tobias Hopp <tobias.hopp@alzura.com>
2025-03-14 02:53:32 +0100

    NewTab Start

--- entrypoints/newtab/App.tsx
+++ entrypoints/newtab/App.tsx
@@ -2,8 +2,8 @@ const App = () => {
   return (
     <div className="min-h-screen bg-rotterdam bg-white/30 bg-cover p-0">
       <div className="min-h-screen backdrop-blur">
-        <div className="max-w-[1200px] mx-auto bg-rose-300">
-          <input type="text" className="w-full" />
+        <div className="max-w-[800px] mx-auto bg-white/30 backdrop rounded-md relative top-64">
+          <input type="text" className="w-full p-4 text-xl rounded-md" />
         </div>
       </div>
     </div>
