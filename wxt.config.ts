c20f060dd3bc1ad2c03275790f394e5f56285558
Tobias Hopp <tobias.hopp@alzura.com>
2025-03-14 22:10:20 +0100

    Assets added

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   extensionApi: "chrome",
-  modules: ["@wxt-dev/module-react"],
+  modules: ["@wxt-dev/module-react", "@wxt-dev/auto-icons"],
   vite: () => ({
     plugins: [tailwindcss()],
   }),
