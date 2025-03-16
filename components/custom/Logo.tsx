dc089e81e58ca918f122a7466fce495ebf8f7bf4
Tobias Hopp <tobias.hopp@alzura.com>
2025-03-16 04:09:34 +0100

    App icon

--- components/custom/Logo.tsx
+++ components/custom/Logo.tsx
@@ -1,8 +1,16 @@
 const Logo = () => {
   return (
-    <h1 className="text-2xl font-medium text-center">
-      <a href="https://webdev-hq.com" target="_blank" title="WebDev HQ">
-        WebDev HQ
+    <h1 className="flex text-3xl text-white">
+      <a href="/" className="flex text-slate-800">
+        <img
+          src="/assets/icons/extension/icon-32.png"
+          className="logo inline mr-2 -mt-1"
+          alt="WebDev HQ Logo"
+        />
+        <span className="font-light">
+          <span className="web">Web</span>
+          <span className="dev-hq font-medium">Dev HQ</span>
+        </span>
       </a>
     </h1>
   );
