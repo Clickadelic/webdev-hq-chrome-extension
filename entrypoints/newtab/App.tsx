d1f0ec35d31aa39b73909e6adae9f891f81e3663
Tobias Hopp <tobias.hopp@alzura.com>
2025-03-16 04:38:48 +0100

    Login form

--- entrypoints/newtab/App.tsx
+++ entrypoints/newtab/App.tsx
@@ -3,7 +3,7 @@ import { AiOutlineSearch } from "react-icons/ai";
 const App = () => {
   return (
     <div className="min-h-screen bg-rotterdam bg-white/30 bg-cover p-0">
-      <div className="min-h-screen backdrop-blur">
+      <div className="min-h-screen backdrop-blur m-3 rounded-lg">
         <div className="max-w-[800px] mx-auto bg-white/30 backdrop rounded-md relative top-64 flex flex-row">
           <input
             type="text"
--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -4,6 +4,33 @@ const App = () => {
   return (
     <div className="size-[600px] bg-slate-100 p-3">
       <Logo wrapperClasses="w-[165px] mx-auto mt-8" />
+      <div className="rounded-md p-4">
+        <h1 className="text-2xl font-light text-center mt-3 mb-6 text-slate-900">
+          Login
+        </h1>
+        <form className="bg-white p-12 rounded-md w-96 flex flex-col space-y-3 mx-auto">
+          <input
+            type="text"
+            className="block border w-full p-2 text-xl rounded-md"
+            placeholder="E-Mail"
+          />
+          <input
+            type="text"
+            className="block border w-full p-2 text-xl rounded-md"
+            placeholder="Password"
+          />
+          <button className="bg-blue-500 text-white p-3 rounded hover:bg-blue-600">
+            Login
+          </button>
+        </form>
+        <a
+          href="https://webdev-hq.com/auth/forgot-password"
+          className="block mt-3 mx-auto text-center text-slate-500 hover:text-blue-600"
+          target="_blank"
+        >
+          Forgot Password
+        </a>
+      </div>
     </div>
   );
 };
