d406d3f17070232c3e7e8cd7d1124f2d6a46cdd7
Tobias Hopp <tobias.hopp@alzura.com>
2025-03-16 04:39:29 +0100

    h1 to h2

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -5,9 +5,9 @@ const App = () => {
     <div className="size-[600px] bg-slate-100 p-3">
       <Logo wrapperClasses="w-[165px] mx-auto mt-8" />
       <div className="rounded-md p-4">
-        <h1 className="text-2xl font-light text-center mt-3 mb-6 text-slate-900">
+        <h2 className="text-2xl font-light text-center mt-3 mb-6 text-slate-900">
           Login
-        </h1>
+        </h2>
         <form className="bg-white p-12 rounded-md w-96 flex flex-col space-y-3 mx-auto">
           <input
             type="text"
