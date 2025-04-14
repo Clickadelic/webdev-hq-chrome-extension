07f1bb4a35c634a1f8253b033dbf5252f210cfbb
clickadelic <toby.hopp@gmail.com>
2025-04-14 23:51:28 +0200

    Options page

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,11 +1,11 @@
-import AppIcon from "@/components/custom/AppIcon"
+import { Button } from "@/components/ui/button"
 
 const App = () => {
 	return (
-		<div className="size-[450px] bg-slate-100 pt-12">
-			<div className="bg-white w-96 rounded-md p-6 mx-auto">
-				<AppIcon url="/" classNames="flex mr-2" />
-			</div>
+		<div className="bg-transparent w-96 rounded-md p-6 mx-auto">
+			<Button className="w-full" onClick={() => chrome.runtime.openOptionsPage()}>
+				Inspect
+			</Button>
 		</div>
 	)
 }
