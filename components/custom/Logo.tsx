7b64707f77039a4579c060863b31f4ee275aafa1
clickadelic <toby.hopp@gmail.com>
2025-04-15 16:15:56 +0200

    Logo fix

--- components/custom/Logo.tsx
+++ components/custom/Logo.tsx
@@ -1,35 +1,5 @@
-<<<<<<< HEAD
-import logoUrl from "@/assets/icons/extension/icon-32.png";
-
-const img = document.createElement("img");
-img.src = logoUrl;
-
-interface LogoProps {
-  headingClasses?: string;
-  classes?: string;
-}
-
-const Logo = ({ headingClasses, classes }: LogoProps = {}) => {
-  return (
-    <h1 className={headingClasses}>
-      <a href={import.meta.env.VITE_HOMEPAGE_URL} className={classes}>
-        <img
-          src={logoUrl}
-          className="logo inline mr-2 -mt-1"
-          alt="WebDev HQ Logo"
-        />
-        <span className="font-light">
-          <span className="web">Web</span>
-          <span className="dev-hq font-medium">Dev HQ</span>
-        </span>
-      </a>
-    </h1>
-  );
-};
-=======
 import logoUrl from "@/assets/icons/extension/icon-32.png"
 import { cn } from "@/lib/utils"
->>>>>>> 30780a54112071245a1ea0008a78d32bfcf7d2cd
 
 const img = document.createElement("img")
 img.src = logoUrl
