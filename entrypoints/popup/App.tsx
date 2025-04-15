4d52432f76ff9685c660c7abf2b950b674dd2bf3
clickadelic <toby.hopp@gmail.com>
2025-04-15 16:14:12 +0200

    package.json fix, dev fix

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,14 +1,6 @@
 import { Button } from "@/components/ui/button"
 
 const App = () => {
-<<<<<<< HEAD
-  return (
-    <div className="size-[600px] bg-slate-100 p-3 pt-8 text-center">
-      <Logo classes="text-3xl" />
-    </div>
-  );
-};
-=======
 	return (
 		<div className="bg-transparent w-96 rounded-md p-6 mx-auto">
 			<Button className="w-full" onClick={() => chrome.runtime.openOptionsPage()}>
@@ -17,6 +9,5 @@ const App = () => {
 		</div>
 	)
 }
->>>>>>> 30780a54112071245a1ea0008a78d32bfcf7d2cd
 
 export default App
