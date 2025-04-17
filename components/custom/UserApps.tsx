d2760fa25df3f592b19c869b274e7bd1bb0044a9
clickadelic <toby.hopp@gmail.com>
2025-04-17 12:45:09 +0200

    Apps mit Zustand

--- components/custom/UserApps.tsx
+++ components/custom/UserApps.tsx
@@ -3,40 +3,46 @@ import { useEffect, useState } from "react"
 import { Plus } from "lucide-react"
 import { BsApp } from "react-icons/bs"
 
-import { apps } from "@/fake-data/apps"
-
 import { Button } from "@/components/ui/button"
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 
+import { useUserAppsStore, UserAppType } from "@/stores/use-user-apps" // Pfad anpassen!
+
 const UserApps = () => {
+	const { userApps, addApp } = useUserAppsStore()
+
 	const addAppLabel = chrome.i18n.getMessage("add_app")
 	const addAppDescription = chrome.i18n.getMessage("add_app_description")
 
 	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
 		const formData = new FormData(e.currentTarget)
-		const appName = formData.get("appName") as string
-		const appUrl = formData.get("appUrl") as string
-		const appIcon = formData.get("appIcon") as string
-		const newApp = { id: crypto.randomUUID(), name: appName, url: appUrl, icon: appIcon }
-		apps.push(newApp)
+		const appName = formData.get("name") as string
+		const appUrl = formData.get("url") as string
+		const newApp: UserAppType = {
+			id: crypto.randomUUID(),
+			name: appName,
+			url: appUrl,
+			icon: "https://www.google.com/s2/favicons?domain=" + appUrl + "&sz=64" // Beispiel für favicon
+		}
+		addApp(newApp)
 		e.currentTarget.reset()
 	}
 
 	return (
 		<ul className="w-full grid grid-cols-9 gap-2">
-			{apps.map(app => (
-				<li key={app.id} className="bg-white p-0 rounded hover:bg-white/70 hover:cursor-pointer">
-					<a href={app.url} target="_blank" className="flex flex-col justify-between items-center p-2 size-[70px]" rel="noopener noreferrer">
-						<img src={app.icon} alt={app.name} className="size-6 mt-1" />
-						<span className="text-slate-800">{app.name}</span>
+			{userApps.map(userApp => (
+				<li key={userApp.id} className="bg-white p-0 rounded hover:bg-white/70 hover:cursor-pointer">
+					<a href={userApp.url} target="_blank" className="flex flex-col justify-between items-center p-2 size-[70px]" rel="noopener noreferrer">
+						<img src={userApp.icon} alt={userApp.name} className="size-6 mt-1" />
+						<span className="text-slate-800">{userApp.name}</span>
 					</a>
 				</li>
 			))}
 			<li>
 				<Dialog>
-					<DialogTrigger className="flex flex-col gap-1 items-center place-content-center bg-white p-2 size-[64px] rounded hover:bg-white/70 hover:cursor-pointer">
+					<DialogTrigger className="flex flex-col gap-1 items-center place-content-center bg-white p-2 size-[70px] rounded hover:bg-white/70 hover:cursor-pointer">
 						<Plus />
 					</DialogTrigger>
 					<DialogContent>
@@ -48,12 +54,7 @@ const UserApps = () => {
 							<DialogDescription>{addAppDescription}</DialogDescription>
 						</DialogHeader>
 						<div className="flex">
-							<form
-								onSubmit={() => {
-									handleSubmit(e)
-								}}
-								className="w-full flex flex-col gap-2"
-							>
+							<form onSubmit={handleSubmit} className="w-full flex flex-col gap-2">
 								<Input name="name" placeholder="AppName" />
 								<Input name="url" placeholder="https://" />
 								<Button type="submit" variant="primary">
