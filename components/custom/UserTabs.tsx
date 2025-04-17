a1997945e98e01b322e0ff8db4679bf3c66f0a1e
clickadelic <toby.hopp@gmail.com>
2025-04-17 14:32:43 +0200

    Todos

--- components/custom/UserTabs.tsx
+++ components/custom/UserTabs.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react"
 
+// TODO: Zusammenfassen, Types, Favicon funktion
 type TabInfo = {
 	id?: number
 	title?: string
--- components/custom/UserTodos.tsx
+++ components/custom/UserTodos.tsx
@@ -3,6 +3,7 @@ import { Button } from "@/components/ui/button"
 import { Plus } from "lucide-react"
 import { Input } from "@/components/ui/input"
 import { BsTrash3 } from "react-icons/bs"
+import { cn } from "@/lib/utils"
 
 type TodosProps = {
 	id: string
@@ -14,6 +15,7 @@ const UserTodos = () => {
 	const newTodoPlaceholder = chrome.i18n.getMessage("new_todo_placeholder")
 	const createFirstTask = chrome.i18n.getMessage("create_your_first_task")
 	const [todos, setTodos] = useState<TodosProps[]>([])
+
 	const handleTodoSubmit = (e: React.FormEvent<HTMLFormElement>) => {
 		e.preventDefault()
 		const formData: FormData = new FormData(e.currentTarget)
@@ -24,7 +26,17 @@ const UserTodos = () => {
 		e.currentTarget.reset()
 	}
 
-	// Todos logik
+	const handleDone = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const todoId = e.target.name
+		const updatedTodos = todos.map(todo => (todo.id === todoId ? { ...todo, done: !todo.done } : todo))
+		setTodos(updatedTodos)
+	}
+
+	const handleDelete = (id: string) => {
+		const updatedTodos = todos.filter(todo => todo.id !== id)
+		setTodos(updatedTodos)
+	}
+
 	return (
 		<div className="flex flex-col bg-white/30 backdrop-blur p-2 rounded">
 			<form className="flex flex-row w-full gap-2" onSubmit={handleTodoSubmit}>
@@ -34,12 +46,12 @@ const UserTodos = () => {
 				</Button>
 			</form>
 			<ul className="w-full flex flex-col space-y-3 mt-3">
-				{todos.length === 0 && <p className="text-center text-md text-slate-800">{createFirstTask}</p>}
+				{todos.length === 0 && <p className="text-center text-md text-white">{createFirstTask}</p>}
 				{todos.map(todo => (
-					<li key={todo.id} className="flex justify-start bg-white rounded p-1">
-						<Input type="checkbox" className="mt-1 size-4" />
-						<span className="w-full">{todo.name}</span>
-						<Button variant="delete" size="sm">
+					<li key={todo.id} className="flex justify-start items-start bg-white rounded p-1">
+						<Input type="checkbox" name={todo.id} checked={todo.done} onChange={handleDone} className="mt-2 mx-2 size-4" />
+						<span className={cn("w-full flex-grow mt-1.5", todo.done && "line-through text-slate-500")}>{todo.name}</span>
+						<Button variant="delete" size="sm" onClick={() => handleDelete(todo.id)} className="mt-1">
 							<BsTrash3 />
 						</Button>
 					</li>
