a4406b2536683afbc15c76f03da8528f8b759cdf
Clickadelic <toby.hopp@gmail.com>
2025-05-16 00:11:21 +0200

    env url

--- entrypoints/newtab/App.tsx
+++ entrypoints/newtab/App.tsx
@@ -11,7 +11,7 @@ import TabsModule from "@/components/newtab/TabsModule"
 const App = () => {
 	return (
 		<BackgroundImage>
-			<AppIcon url="https://webdev-hq.com" classNames="absolute top-4 left-4 text-white z-50" target="_blank" />
+			<AppIcon url={`${import.meta.env.WXT_HOMEPAGE_URL}`} classNames="absolute top-4 left-4 text-white z-50" target="_blank" />
 			<Clock classNames="mt-4 mx-auto" digitStyle="text-4xl text-white font-light" />
 			<AccountMenu classNames="absolute top-4 right-4 text-white" />
 			<Salutation classNames="w-[680px] mt-32 mb-4 mx-auto text-white text-4xl font-light" />
