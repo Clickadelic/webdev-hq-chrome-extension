286a1327ef141628a78c7bde15a65275eb4deab1
Clickadelic <toby.hopp@gmail.com>
2025-08-06 01:27:02 +0200

    React DnD Kit sortable App tile working

--- components/newtab/tabs-module/SortableAppTile.tsx
+++ components/newtab/tabs-module/SortableAppTile.tsx
@@ -1,9 +1,10 @@
 import { useSortable } from "@dnd-kit/sortable"
 import { CSS } from "@dnd-kit/utilities"
-import { HiOutlineDotsVertical } from "react-icons/hi"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
-import { cn } from "@/lib/utils"
 
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { AiOutlineEdit } from "react-icons/ai"
+import { BsTrash } from "react-icons/bs"
+import { HiOutlineDotsVertical } from "react-icons/hi"
 export interface SortableAppTileProps {
 	app: {
 		id: string
@@ -17,7 +18,7 @@ export interface SortableAppTileProps {
 	setWasDragged: (val: boolean) => void
 }
 
-export const SortableAppTile = ({ app, onEdit, onDelete, wasDragged, setWasDragged }: SortableAppTileProps) => {
+export const SortableAppTile = ({ app, onEdit, onDelete }: SortableAppTileProps) => {
 	const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id: app.id })
 
 	const style = {
@@ -26,36 +27,43 @@ export const SortableAppTile = ({ app, onEdit, onDelete, wasDragged, setWasDragg
 	}
 
 	return (
-		<li ref={setNodeRef} style={style} {...attributes} {...listeners}>
-			<a
-				href={wasDragged ? undefined : app.url}
-				onClick={e => {
-					if (wasDragged) {
-						e.preventDefault()
-						setWasDragged(false)
-					}
-				}}
-				target="_self"
-				rel="noopener noreferrer"
-				className={cn(
-					"group flex flex-col justify-between items-center p-2 gap-2 text-xs bg-white dark:bg-slate-800 border border-transparent rounded hover:border-mantis-primary hover:text-mantis-primary transition-colors duration-150 ease-in-out cursor-pointer size-[70px]",
-					isDragging && "opacity-50"
-				)}
-			>
-				<img src={app.icon} alt={app.title} className="size-5" />
-				<span className="text-center leading-tight line-clamp-2">{app.title}</span>
-				<DropdownMenu>
-					<DropdownMenuTrigger asChild>
-						<div className="absolute top-1.5 right-1.5 text-slate-400 hover:text-mantis-primary">
-							<HiOutlineDotsVertical />
-						</div>
-					</DropdownMenuTrigger>
-					<DropdownMenuContent align="end">
-						<DropdownMenuItem onClick={() => onEdit(app.id)}>{chrome.i18n.getMessage("edit")}</DropdownMenuItem>
-						<DropdownMenuItem onClick={() => onDelete(app.id)}>{chrome.i18n.getMessage("delete")}</DropdownMenuItem>
-					</DropdownMenuContent>
-				</DropdownMenu>
+		<li ref={setNodeRef}
+			style={style}
+			key={app.id}
+			
+			className="size-[70px] z-10 relative bg-white dark:bg-slate-800 pt-1 rounded border-1 transition-colors duration-150 ease-in-out border-transparent hover:border-mantis-primary hover:text-mantis-primary hover:cursor-pointer"
+		>
+			<div
+				className="drag-handle absolute top-[4px] left-[24px] w-[20px] h-[5px] bg-slate-200 dark:bg-slate-700 dark:hover:bg-slate-600 inline-flex z-100 text-slate-600 dark:text-slate-300 rounded hover:cursor-move"
+				{...listeners}
+				{...attributes}
+			/>
+			<a href={app.url} target="_self" className="flex flex-col justify-between items-center p-2 gap-2" rel="noopener noreferrer">
+				<img src={app.icon} alt={app.title} className="size-6 rounded-xs" />
+				<span className="text-slate-800 dark:text-slate-300 text-xs inline-block truncate max-w-[56px]">{app.title}</span>
 			</a>
+			<DropdownMenu modal={false}>
+				<DropdownMenuTrigger asChild>
+					<button className="z-50 absolute top-1 right-[1px] text-slate-600 dark:text-slate-300 rounded-xs hover:text-slate-900 hover:bg-slate-200 hover:cursor-pointer dark:hover:bg-slate-600">
+						<HiOutlineDotsVertical className="size-4" />
+					</button>
+				</DropdownMenuTrigger>
+				
+				<DropdownMenuContent side="right" align="start" className="rounded">
+					<DropdownMenuItem>
+						<button onClick={() => onEdit(app.id)} className="flex justify-between rounded">
+							<AiOutlineEdit className="mt-1 mr-2" />
+							{chrome.i18n.getMessage("edit", "Edit")}
+						</button>
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<button onClick={() => onDelete(app.id)} className="flex justify-between text-red-500 hover:text-red-700 rounded">
+							<BsTrash className="text-red-500 hover:text-red-700 size-3 mt-1 mr-2" />
+							{chrome.i18n.getMessage("delete", "Delete")}
+						</button>
+					</DropdownMenuItem>
+				</DropdownMenuContent>
+			</DropdownMenu>
 		</li>
 	)
 }
--- components/newtab/tabs-module/UserApps.tsx
+++ components/newtab/tabs-module/UserApps.tsx
@@ -21,6 +21,12 @@ import { FormError } from "@/components/global/forms/form-error"
 import { FormSuccess } from "@/components/global/forms/form-success"
 import { toast } from "sonner"
 import { getFaviconUrl } from "@/lib/utils"
+import type { DragStartEvent } from '@dnd-kit/core'
+
+import { SortableAppTile } from "./SortableAppTile"
+import type { DragEndEvent } from "@dnd-kit/core"
+import { DndContext, closestCenter, PointerSensor, useSensor, useSensors, DragOverlay } from "@dnd-kit/core"
+import { arrayMove, SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable"
 
 const UserApps = () => {
 	const { apps, addApp, editApp, removeApp } = useAppStore()
@@ -30,11 +36,45 @@ const UserApps = () => {
 	const [editingAppId, setEditingAppId] = useState<string | null>(null)
 	const [error, setError] = useState<string | undefined>("")
 	const [success, setSuccess] = useState<string | undefined>("")
+	const [draggingApp, setDraggingApp] = useState<AppType | null>(null)
+	const sensors = useSensors(
+		useSensor(PointerSensor, {
+			activationConstraint: {
+				distance: 8, // minimale Bewegung zum Starten von Drag (z.B. um Klick vs. Drag zu unterscheiden)
+			},
+		})
+	)
+
+	const handleDragStart = (event: DragStartEvent) => {
+		const activeId = event.active.id
+
+		const found = apps.find(app => app.id === activeId)
+		if (found) {
+			setDraggingApp(found)
+		}
+	}
+
+
+	const handleDragEnd = ({ active, over }: DragEndEvent) => {
+		setDraggingApp(null)
+
+		if (!over || active.id === over.id) return
+
+		const oldIndex = apps.findIndex(app => app.id === active.id)
+		const newIndex = apps.findIndex(app => app.id === over.id)
+
+		if (oldIndex !== -1 && newIndex !== -1) {
+			const newApps = arrayMove(apps, oldIndex, newIndex)
+			useAppStore.getState().reorderApps(newApps)
+		}
+	}
+
 
 	let form = useForm<z.infer<typeof AppSchema>>({
 		resolver: zodResolver(AppSchema),
 		defaultValues: { title: "", url: "" }
 	})
+
 	const onAddSubmit = (values: z.infer<typeof AppSchema>) => {
 		setError("")
 		setSuccess("")
@@ -99,110 +139,102 @@ const UserApps = () => {
 	}
 
 	return (
-		<ul className="w-full grid grid-cols-12 gap-1 p-1 bg-white/30 dark:bg-slate-800/30 rounded backdrop-blur">
-			{apps.map(app => (
-				<li
-					key={app.id}
-					className="size-[70px]  relative bg-white dark:bg-slate-800 pt-1 rounded border-1 transition-colors duration-150 ease-in-out border-transparent hover:border-mantis-primary hover:text-mantis-primary hover:cursor-pointer"
-				>
-					<a href={app.url} target="_self" className="flex flex-col justify-between items-center p-2 gap-2" rel="noopener noreferrer">
-						<img src={app.icon} alt={app.title} className="size-6 rounded-xs" />
-						<span className="text-slate-800 dark:text-slate-300 text-xs inline-block truncate max-w-[56px]">{app.title}</span>
-					</a>
-					<DropdownMenu modal={false}>
-						<DropdownMenuTrigger asChild>
-							<button className="absolute top-1 right-[1px] text-slate-600 dark:text-slate-300 rounded-xs hover:text-slate-900 hover:bg-slate-200 hover:cursor-pointer dark:hover:bg-slate-600">
-								<HiOutlineDotsVertical className="size-4" />
-							</button>
-						</DropdownMenuTrigger>
-						<DropdownMenuContent side="right" align="start" className="rounded">
-							<DropdownMenuItem>
-								<button onClick={() => onEdit(app.id)} className="flex justify-between rounded">
-									<AiOutlineEdit className="mt-1 mr-2" />
-									{chrome.i18n.getMessage("edit", "Edit")}
-								</button>
-							</DropdownMenuItem>
-							<DropdownMenuItem>
-								<button onClick={() => onDelete(app.id)} className="flex justify-between text-red-500 hover:text-red-700 rounded">
-									<BsTrash className="text-red-500 hover:text-red-700 size-3 mt-1 mr-2" /> {chrome.i18n.getMessage("delete", "Delete")}
-								</button>
-							</DropdownMenuItem>
-						</DropdownMenuContent>
-					</DropdownMenu>
-				</li>
-			))}
-			<li>
-				<Dialog
-					open={isModalOpen}
-					onOpenChange={open => {
-						setIsModalOpen(open)
-						if (!open) {
-							setIsEditing(false)
-							setEditingAppId(null)
-							form.reset()
-						}
-					}}
-				>
-					<DialogTrigger
-						onClick={() => setIsModalOpen(true)}
-						className="flex flex-col gap-1 items-center place-content-center text-slate-400 dark:text-slate-300 bg-white dark:bg-slate-800 p-2 size-[70px] rounded border-1 transition-colors duration-150 ease-in-out border-transparent hover:border-mantis-primary hover:text-mantis-primary hover:cursor-pointer"
-					>
-						<Plus />
-					</DialogTrigger>
-					<DialogContent className="rounded">
-						<DialogHeader>
-							<DialogTitle className="flex items-start gap-2">
-								<BsApp />
-								{isEditing ? chrome.i18n.getMessage("edit_app_title", "Edit App") : chrome.i18n.getMessage("add_app_title", "Add App")}
-							</DialogTitle>
-							<DialogDescription>
-								{isEditing ? chrome.i18n.getMessage("edit_app_description") : chrome.i18n.getMessage("add_app_description", "Add a new app to your list.")}
-							</DialogDescription>
-						</DialogHeader>
-						<div className="flex">
-							<Form {...form}>
-								<form onSubmit={form.handleSubmit(isEditing ? onEditSubmit : onAddSubmit)} className="w-full space-y-6">
-									<div className="space-y-4">
-										<FormField
-											control={form.control}
-											name="title"
-											render={({ field }) => (
-												<FormItem>
-													<FormLabel>{chrome.i18n.getMessage("app_title", "Title")}:</FormLabel>
-													<FormControl>
-														<Input type="text" {...field} placeholder={chrome.i18n.getMessage("app_title_placeholder", "Title")} />
-													</FormControl>
-													<FormMessage />
-												</FormItem>
-											)}
-										/>
-										<FormField
-											control={form.control}
-											name="url"
-											render={({ field }) => (
-												<FormItem>
-													<FormLabel>{chrome.i18n.getMessage("app_url", "Url")}:</FormLabel>
-													<FormControl>
-														<Input type="url" {...field} placeholder={chrome.i18n.getMessage("app_url_placeholder", "https://...")} />
-													</FormControl>
-													<FormMessage />
-												</FormItem>
-											)}
-										/>
-									</div>
-									<FormError message={error} />
-									<FormSuccess message={success} />
-									<Button variant="primary" type="submit" className="w-full rounded" disabled={isLoading}>
-										{isEditing ? <TbEdit /> : <Plus />}
-										{isEditing ? chrome.i18n.getMessage("edit_app", "Edit app") : chrome.i18n.getMessage("add_app", "Add app")}
-									</Button>
-								</form>
-							</Form>
-						</div>
-					</DialogContent>
-				</Dialog>
-			</li>
-		</ul>
+		<DndContext sensors={sensors} collisionDetection={closestCenter} onDragEnd={handleDragEnd} onDragStart={handleDragStart}>
+			<SortableContext items={apps.map(app => app.id)} strategy={verticalListSortingStrategy}>
+				<ul className="w-full grid grid-cols-12 gap-1 p-1 bg-white/30 dark:bg-slate-800/30 rounded backdrop-blur">
+					{apps.map(app => (
+						<SortableAppTile
+							key={app.id}
+							app={app}
+							onEdit={onEdit}
+							onDelete={onDelete}
+							wasDragged={isEditing}
+							
+							setWasDragged={setIsEditing}
+						/>
+					))}
+					<li>
+						<Dialog
+							open={isModalOpen}
+							onOpenChange={open => {
+								setIsModalOpen(open)
+								if (!open) {
+									setIsEditing(false)
+									setEditingAppId(null)
+									form.reset()
+								}
+							}}
+						>
+							<DialogTrigger
+								onClick={() => setIsModalOpen(true)}
+								className="flex flex-col gap-1 items-center place-content-center text-slate-400 dark:text-slate-300 bg-white dark:bg-slate-800 p-2 size-[70px] rounded border-1 transition-colors duration-150 ease-in-out border-transparent hover:border-mantis-primary hover:text-mantis-primary hover:cursor-pointer"
+							>
+								<Plus />
+							</DialogTrigger>
+							<DialogContent className="rounded">
+								<DialogHeader>
+									<DialogTitle className="flex items-start gap-2">
+										<BsApp />
+										{isEditing ? chrome.i18n.getMessage("edit_app_title", "Edit App") : chrome.i18n.getMessage("add_app_title", "Add App")}
+									</DialogTitle>
+									<DialogDescription>
+										{isEditing ? chrome.i18n.getMessage("edit_app_description") : chrome.i18n.getMessage("add_app_description", "Add a new app to your list.")}
+									</DialogDescription>
+								</DialogHeader>
+								<div className="flex">
+									<Form {...form}>
+										<form onSubmit={form.handleSubmit(isEditing ? onEditSubmit : onAddSubmit)} className="w-full space-y-6">
+											<div className="space-y-4">
+												<FormField
+													control={form.control}
+													name="title"
+													render={({ field }) => (
+														<FormItem>
+															<FormLabel>{chrome.i18n.getMessage("app_title", "Title")}:</FormLabel>
+															<FormControl>
+																<Input type="text" {...field} placeholder={chrome.i18n.getMessage("app_title_placeholder", "Title")} />
+															</FormControl>
+															<FormMessage />
+														</FormItem>
+													)}
+												/>
+												<FormField
+													control={form.control}
+													name="url"
+													render={({ field }) => (
+														<FormItem>
+															<FormLabel>{chrome.i18n.getMessage("app_url", "Url")}:</FormLabel>
+															<FormControl>
+																<Input type="url" {...field} placeholder={chrome.i18n.getMessage("app_url_placeholder", "https://...")} />
+															</FormControl>
+															<FormMessage />
+														</FormItem>
+													)}
+												/>
+											</div>
+											<FormError message={error} />
+											<FormSuccess message={success} />
+											<Button variant="primary" type="submit" className="w-full rounded" disabled={isLoading}>
+												{isEditing ? <TbEdit /> : <Plus />}
+												{isEditing ? chrome.i18n.getMessage("edit_app", "Edit app") : chrome.i18n.getMessage("add_app", "Add app")}
+											</Button>
+										</form>
+									</Form>
+								</div>
+							</DialogContent>
+						</Dialog>
+					</li>
+				</ul>
+			</SortableContext>
+			<DragOverlay zIndex={50}>
+				{draggingApp ? (
+					<div className="size-[70px] bg-white dark:bg-slate-800 pt-1 rounded border border-mantis-primary shadow-lg p-2 flex flex-col items-center justify-between">
+						<img src={draggingApp.icon} alt={draggingApp.title} className="size-6 rounded-xs" />
+						<span className="text-slate-800 dark:text-slate-300 text-xs truncate max-w-[56px]">{draggingApp.title}</span>
+					</div>
+				) : null}
+			</DragOverlay>
+		</DndContext>
 	)
 }
 
