2c3b21a9f127a92f4812fd3b2386effd54067b81
Clickadelic <toby.hopp@gmail.com>
2026-02-19 02:02:24 +0100

    YouTube Search Engine added, TODO WIP

--- components/newtab/tabs-module/TodoList.tsx
+++ components/newtab/tabs-module/TodoList.tsx
@@ -3,7 +3,7 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
 
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
-import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog"
+
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
@@ -22,35 +22,21 @@ import { TodoSchema } from "@/schemas"
 import { useState } from "react"
 import { TbEdit } from "react-icons/tb"
 import { BsListCheck } from "react-icons/bs"
-// import { DatePickerButton } from "@/components/global/DatepickerButton"
+import { DatePickerButton } from "@/components/global/DatepickerButton"
 
 const TodoList = () => {
 	const { todos, addTodo, toggleTodo, deleteTodo } = useTodoStore()
 
-	const [isModalOpen, setIsModalOpen] = useState<boolean>(false)
 	const [isLoading, setIsLoading] = useState<boolean>(false)
 	const [isEditing, setIsEditing] = useState<boolean>(false)
 	const [editingTodoId, setEditingTodoId] = useState<string | null>(null)
 	const [error, setError] = useState<string | undefined>("")
 	const [success, setSuccess] = useState<string | undefined>("")
-	const [quickFormError, setQuickFormError] = useState<string | undefined>("")
-	const [quickFormSuccess, setQuickFormSuccess] = useState<string | undefined>("")
-
-	const QuickFormSchema = TodoSchema.pick({ title: true })
-
-	const quickForm = useForm<z.infer<typeof QuickFormSchema>>({
-		resolver: zodResolver(QuickFormSchema),
-		defaultValues: { title: "" }
-	})
 
 	const form = useForm<z.infer<typeof TodoSchema>>({
 		resolver: zodResolver(TodoSchema),
 		defaultValues: { title: "", description: "", done: false }
 	})
-
-	const openAdvancedAdd = () => {
-		setIsModalOpen(true)
-	}
 
 	const onAddSubmit = (values: z.infer<typeof TodoSchema>) => {
 		setError("")
@@ -62,19 +48,6 @@ const TodoList = () => {
 		quickForm.reset()
 		setIsModalOpen(false)
 		toast.success(chrome.i18n.getMessage("todo_added", "Todo added."))
-	}
-
-	const onQuickAddSubmit = (values: z.infer<typeof QuickFormSchema>) => {
-		try {
-			addTodo(values.title)
-			setQuickFormSuccess("")
-			setQuickFormError("")
-			quickForm.reset()
-			toast.success(chrome.i18n.getMessage("todo_added", "Todo added."))
-		} catch (e) {
-			setQuickFormError("Something went wrong.")
-			setQuickFormSuccess("")
-		}
 	}
 
 	const onEdit = (id: string) => {
@@ -123,11 +96,11 @@ const TodoList = () => {
 	return (
 		<div className="flex flex-col bg-white/30 dark:bg-slate-800/30 backdrop-blur p-1 space-y-1 rounded">
 			<div className="bg-white  dark:bg-slate-800 rounded p-1 backdrop-blur">
-				<Form {...quickForm}>
-					<form onSubmit={quickForm.handleSubmit(onQuickAddSubmit)} className="flex flex-row w-full gap-1">
+				<Form {...form}>
+					<form onSubmit={form.handleSubmit(onAddSubmit)} className="flex flex-row w-full gap-1">
 						<div className="flex flex-row w-full gap-1">
 							<FormField
-								control={quickForm.control}
+								control={form.control}
 								name="title"
 								render={({ field }) => (
 									<FormItem className="w-full flex flex-col">
@@ -145,91 +118,7 @@ const TodoList = () => {
 									</FormItem>
 								)}
 							/>
-							{/* <DatePickerButton /> */}
-							<Dialog
-								open={isModalOpen}
-								onOpenChange={open => {
-									setIsModalOpen(open)
-									if (!open) {
-										setIsEditing(false)
-										setEditingTodoId(null)
-										form.reset()
-										quickForm.reset()
-									}
-								}}
-							>
-								<DialogTrigger
-									onClick={() => openAdvancedAdd()}
-									className="h-[36px] hover:bg-slate-100 text-slate-400 hover:text-slate-400 dark:hover:text-slate-400 dark:bg-slate-700 border border-transparent flex items-center justify-center py-2 px-3 rounded hover:cursor-pointer dark:hover:bg-slate-600"
-								>
-									<FaArrowUpRightDots className="size-4" />
-								</DialogTrigger>
-								<DialogContent className="rounded">
-									<DialogHeader>
-										<DialogTitle className="flex items-start gap-2">
-											<BsListCheck className="mt-[2px] size-4" />
-											{isEditing ? chrome.i18n.getMessage("edit_todo_title", "Edit Todo") : chrome.i18n.getMessage("add_todo_title", "Add todo")}
-										</DialogTitle>
-										<DialogDescription>
-											{isEditing ? chrome.i18n.getMessage("edit_todo_description") : chrome.i18n.getMessage("add_todo_description", "Add a new task to your list.")}
-										</DialogDescription>
-									</DialogHeader>
-									<div className="flex">
-										<Form {...form}>
-											<form onSubmit={form.handleSubmit(isEditing ? onEditSubmit : onAddSubmit)} className="w-full space-y-6">
-												<div className="space-y-4">
-													<FormField
-														control={form.control}
-														name="title"
-														render={({ field }) => (
-															<FormItem>
-																<FormLabel>{chrome.i18n.getMessage("new_todo_title", "Title")}:</FormLabel>
-																<FormControl>
-																	<Input type="text" {...field} placeholder={chrome.i18n.getMessage("new_todo_placeholder", "Todo")} />
-																</FormControl>
-																<FormMessage />
-															</FormItem>
-														)}
-													/>
-													<FormField
-														control={form.control}
-														name="description"
-														render={({ field }) => (
-															<FormItem>
-																<FormLabel>{chrome.i18n.getMessage("new_todo_description_label", "Description")}:</FormLabel>
-																<FormControl>
-																	<Textarea {...field} placeholder={chrome.i18n.getMessage("new_todo_description_placeholder", "Description")} />
-																</FormControl>
-																<FormMessage />
-															</FormItem>
-														)}
-													/>
-
-													<FormField
-														control={form.control}
-														name="done"
-														render={({ field }) => (
-															<FormItem className="flex flex-row flex-start">
-																<FormLabel>{chrome.i18n.getMessage("done_label", "Done")}:</FormLabel>
-																<FormControl>
-																	<Switch className="ml-3" checked={field.value} onCheckedChange={field.onChange} />
-																</FormControl>
-																<FormMessage />
-															</FormItem>
-														)}
-													/>
-												</div>
-												<FormError message={error} />
-												<FormSuccess message={success} />
-												<Button variant="primary" type="submit" className="w-full rounded" disabled={isLoading}>
-													{isEditing ? <TbEdit /> : <Plus />}
-													{isEditing ? chrome.i18n.getMessage("edit_todo", "Edit todo") : chrome.i18n.getMessage("add_todo", "Add todo")}
-												</Button>
-											</form>
-										</Form>
-									</div>
-								</DialogContent>
-							</Dialog>
+							<DatePickerButton />
 							<Button
 								type="submit"
 								className="bg-mantis-primary hover:bg-mantis-primary-hover text-white dark:bg-mantis-primary dark:hover:bg-mantis-primary-hover dark:text-slate-200 rounded py-2 px-3 hover:cursor-pointer"
@@ -238,8 +127,8 @@ const TodoList = () => {
 								{isEditing ? <TbEdit /> : <Plus />}
 							</Button>
 						</div>
-						{quickFormError && <FormError message={quickFormError} />}
-						{quickFormSuccess && <FormSuccess message={quickFormSuccess} />}
+						{error && <FormError message={error} />}
+						{success && <FormSuccess message={success} />}
 					</form>
 				</Form>
 			</div>
@@ -251,8 +140,10 @@ const TodoList = () => {
 				)}
 				{todos.map(todo => (
 					<li key={todo.id} className="flex justify-start items-start p-.5 hover:text-slate-500 dark:text-slate-300 space-x-1">
-						<Input type="checkbox" name={todo.id} checked={todo.done} onChange={() => toggleTodo(todo.id)} className="mt-2 mx-2 size-4 hover:cursor-pointer " />
-						<span className={cn("w-full flex-grow mt-1.5 ", todo.done && "line-through text-slate-500 dark:text-slate-300")}>{todo.title}</span>
+						<div className="w-full flex justify-start hover:bg-gray-100 dark:hover:bg-slate-700 rounded">
+							<Input type="checkbox" name={todo.id} checked={todo.done} onChange={() => toggleTodo(todo.id)} className="mt-2 mx-2 size-4 hover:cursor-pointer " />
+							<span className={cn("w-full grow my-2 ", todo.done && "line-through text-slate-500 dark:text-slate-300")}>{todo.title}</span>
+						</div>
 						<Button variant="ghost" className="text-slate-400" size="sm" onClick={() => onEdit(todo.id)}>
 							<TbEdit className="size-4" />
 						</Button>
--- lib/search-engines.tsx
+++ lib/search-engines.tsx
@@ -3,11 +3,13 @@ import bingSearchIconUrl from "@/assets/icons/search-engines/bing-logo.svg"
 import braveSearchIconUrl from "@/assets/icons/search-engines/brave-logo.svg"
 import duckduckgoSearchIconUrl from "@/assets/icons/search-engines/duck-duck-go-logo.svg"
 import yahooSearchIconUrl from "@/assets/icons/search-engines/yahoo-logo.svg"
+import youtubeSearchIconUrl from "@/assets/icons/search-engines/youtube-logo.svg"
 
 export const engines = [
-	{ name: "Brave", url: "https://search.brave.com/search?q=", icon: <img src={braveSearchIconUrl} className="size-5" alt="Brave Logo" /> },
 	{ name: "Bing", url: "https://www.bing.com/search?q=", icon: <img src={bingSearchIconUrl} className="size-5" alt="Bing Logo" /> },
+	{ name: "Brave", url: "https://search.brave.com/search?q=", icon: <img src={braveSearchIconUrl} className="size-5" alt="Brave Logo" /> },
 	{ name: "DuckDuckGo", url: "https://www.duckduckgo.com/?q=", icon: <img src={duckduckgoSearchIconUrl} className="size-5" alt="DuckDuckGo Logo" /> },
 	{ name: "Google", url: "https://www.google.com/search?q=", icon: <img src={googleSearchIconUrl} className="size-5" alt="Google Logo" /> },
-	{ name: "Yahoo", url: "https://search.yahoo.com/search?p=", icon: <img src={yahooSearchIconUrl} className="size-5" alt="Yahoo Logo" /> }
+	{ name: "Yahoo", url: "https://search.yahoo.com/search?p=", icon: <img src={yahooSearchIconUrl} className="size-5" alt="Yahoo Logo" /> },
+	{ name: "YouTube", url: "https://www.youtube.com/results?search_query=", icon: <img src={youtubeSearchIconUrl} className="size-5" alt="YouTube Logo" /> }
 ]
